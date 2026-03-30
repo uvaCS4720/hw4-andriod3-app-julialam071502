628d3b008958e4c1038d8b25a954f0321ddcd41c
julialam071502 <czg9kd@virginia.edu>
2026-03-29 21:53:40 -0400

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,17 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
-    impleme
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+
+    implementation(libs.maps.compose)
+    implementation(libs.play.services.maps)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
